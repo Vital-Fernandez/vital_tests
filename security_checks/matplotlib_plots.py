997d42ae284cfb8f82285b264db6089e48a5bced
vital-fernandez <vital.fernandez@gmail.com>
2021-08-12 23:58:00 -0400

    Work 2021/08/12

--- security_checks/matplotlib_plots.py
+++ security_checks/matplotlib_plots.py
@@ -1,9 +1,14 @@
+import sys
+import numpy as np
 import matplotlib.pyplot as plt
 
+x = np.array([1, 2, 3, 4])
+y = np.array([1, 2, 3, 4])
+
+label = 'python {}.{}'.format(sys.version_info.major, sys.version_info.minor)
+
 fig, ax = plt.subplots()
-ax.plot(lm.wave, lm.flux, label='Blue arm')
-ax.scatter(lm.wave[idcsLinePeak], lm.flux[idcsLinePeak], label='linePeaks', facecolors='tab:green')
-ax.plot(wave_blue, flux_blue, label='Blue arm')
+ax.plot(x, y, label=label)
 ax.legend()
 ax.update({'xlabel':'Wavelength', 'ylabel':'Flux', 'title':'Gaussian fitting'})
 plt.show()
